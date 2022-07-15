c3b0e28bcbb07bc9cbd777991cab37f681a5b118
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2022-07-15 13:39:48 +0430

    add io, mtr, and nslookup commands

---
+++ ip.sh
@@ -0,0 +1,14 @@
+# Display information about all IP addresses
+#ip addr show
+
+# Display information about a single network interface
+#ip addr show dev wlp3s0:
+
+# Display information about network interfaces
+#ip link show
+
+# Display routing table
+#ip route
+#ip r
+#ip route list
+#ip route list SELECTOR
---
+++ mtr.sh
@@ -0,0 +1,4 @@
+#The mtr command is a combination of ping and traceroute commands.
+#It is a network diagnostic tool that continuously sends packets showing ping time for each hop.
+#It also displays network problems of the entire route taken by the network packets.
+mtr cafebazaar.ir
