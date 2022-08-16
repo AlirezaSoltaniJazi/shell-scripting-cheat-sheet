dacc9fe3cabfaff378c0c8671cbd5d24f79898c7
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2022-08-16 05:02:32 +0430

    Add dig sh

---
+++ dig.sh
@@ -0,0 +1,4 @@
+#dig is a network administration command-line tool for querying the Domain Name System.
+#dig is useful for network troubleshooting and for educational purposes.
+#It can operate based on command line option and flag arguments, or in batch mode by reading requests from an operating system file.
+dig cafebazaar.ir
